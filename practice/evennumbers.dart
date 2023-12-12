884906c5272a26413fa43e1b3ccb1cf9b381626d
Salmanufaris <farisvt768@gmail.com>
2023-12-12 12:25:30 +0530

    second commit

---
+++ practice/evennumbers.dart
@@ -0,0 +1,11 @@
+void main() {
+  List a = [1, 2, 3, 4, 5, 6];
+  for (var number in a) {
+    if (number % 2 == 0) {
+      print(number);
+    }
+  }
+}
+
+
+//finding even numbers in simple way//
